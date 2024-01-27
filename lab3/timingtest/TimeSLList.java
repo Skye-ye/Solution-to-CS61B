eca73bc27ba90e79b9d4026dd2c7a94f0a4dcc4a
Skye-ye <saber.ap.saikou@gmail.com>
2024-01-27 21:29:08 +0800

    Done TimeSLList in Lab3

--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -7,7 +7,7 @@ import edu.princeton.cs.algs4.Stopwatch;
 public class TimeSLList {
     private static void printTimingTable(AList<Integer> Ns, AList<Double> times, AList<Integer> opCounts) {
         System.out.printf("%12s %12s %12s %12s\n", "N", "time (s)", "# ops", "microsec/op");
-        System.out.printf("------------------------------------------------------------\n");
+        System.out.print("------------------------------------------------------------\n");
         for (int i = 0; i < Ns.size(); i += 1) {
             int N = Ns.get(i);
             double time = times.get(i);
@@ -22,7 +22,28 @@ public class TimeSLList {
     }
 
     public static void timeGetLast() {
-        // TODO: YOUR CODE HERE
+        AList<Integer> Ns = new AList<>();
+        AList<Double> times = new AList<>();
+        AList<Integer> opCounts = new AList<>();
+        int[] N = {1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000};
+        int ops = 10000;
+        for (int k : N) {
+            Ns.addLast(k);
+        }
+        for (int i = 0; i < Ns.size(); i++) {
+            SLList<Integer> test = new SLList<>();
+            for (int j = 0; j < Ns.get(i); j++) {
+                test.addLast(j);
+            }
+            Stopwatch sw = new Stopwatch();
+            for (int j = 0; j < ops; j++) {
+                test.getLast();
+            }
+            double timeInSeconds = sw.elapsedTime();
+            times.addLast(timeInSeconds);
+            opCounts.addLast(ops);
+        }
+        printTimingTable(Ns, times, opCounts);
     }
 
 }
